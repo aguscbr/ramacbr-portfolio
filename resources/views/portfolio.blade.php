b1d89e65465e67acad8706215bfe039ad764954d
RamaCBR <aguscbr.dev@gmail.com>
2026-05-16 03:23:34 -0300

    feat: add portfolio frontend view with all sections

---
+++ resources/views/portfolio.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.app>
+    @include('partials.hero')
+    @include('partials.projects')
+    @include('partials.skills')
+    @include('partials.experience')
+    @include('partials.contact')
+</x-layouts.app>
